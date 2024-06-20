25eb790032b650a56f59235b1d26e8f24ba90acb
abdus251 <abdus251@gmail.com>
2024-06-21 05:53:05 +0600

    gradientBg has been added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,5 @@
+import { FaLocationArrow } from 'react-icons/fa6'
+import MagicButton from './ui/MagicButton'
 import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
 
@@ -22,13 +24,20 @@ const Hero = () => {
 
           <TextGenerateEffect
             className='text-center text-[40px] md:text-5xl lg:text-6xl'
-            words='Transforming Concepts into Seamless Experiences' 
+            words='Transforming Concepts into Seamless User Experiences' 
             />
 
             <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'> 
               Hi, I'm Abdus, a Next.js Developer based in Bangladesh.
-
             </p>
+
+            <a href="#about">
+              <MagicButton 
+                title="Show my work"
+                icon={<FaLocationArrow />}
+                position='right'
+                />
+            </a>
         </div>
 
       </div>
